d7942ec895d14c6bb45ecc9a49acd0f3e0af5ff3
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-10-12 13:00:14 +0100

    Refactor: groupID

--- indexing-service/src/main/java/com/tahs/Main.java
+++ indexing-service/src/main/java/com/tahs/Main.java
@@ -1,4 +1,4 @@
-package thealmosthonorstudents;
+package com.tahs;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
