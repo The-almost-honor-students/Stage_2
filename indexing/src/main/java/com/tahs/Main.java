f5e2954fceef4025ed08e11eea2c94cb843790ce
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-10-12 14:14:55 +0100

    Created endpoint in indexing POST

--- indexing/src/main/java/com/tahs/Main.java
+++ indexing/src/main/java/com/tahs/Main.java
@@ -3,9 +3,16 @@ package com.tahs;
 import io.javalin.Javalin;
 import com.google.gson.Gson;
 
+
+import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+    private static final AtomicInteger booksIndexed = new AtomicInteger(0);
+    private static LocalDateTime lastUpdate = LocalDateTime.now();
+    private static double indexSizeMB = 0.0;
+
     public static void main(String[] args) {
         Javalin app = Javalin.create(config -> {
             config.http.defaultContentType = "application/json";
@@ -26,6 +33,22 @@ public class Main {
                     "data", "running"
             );
             ctx.result(gson.toJson(data));
+        });
+
+        app.post("/index/update/{book_id}", ctx -> {
+            String bookId = ctx.pathParam("book_id");
+
+            System.out.println("Indexing book " + bookId + "...");
+            booksIndexed.incrementAndGet();
+            lastUpdate = LocalDateTime.now();
+            indexSizeMB += Math.random() * 0.5;
+
+            Map<String, Object> response = Map.of(
+                    "book_id", bookId,
+                    "index", "updated"
+            );
+
+            ctx.result(gson.toJson(response));
         });
 
     }
