1bdac1508a17fe5f9758a4cf086d394a16b3d7f2
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-10-26 00:12:05 +0100

    refactor Path sumDir

--- indexing/src/main/java/com/tahs/analysis/PlotIndexingBench.java
+++ indexing/src/main/java/com/tahs/analysis/PlotIndexingBench.java
@@ -21,7 +21,7 @@ public class PlotIndexingBench {
     public static void main(String[] args) throws Exception {
         Path csvPath = Path.of(args.length > 0 ? args[0] : "benchmarking_results/indexing_data.csv");
         Path outDir  = Path.of("benchmarking_results/indexing/plots");
-        Path sumDir  = Path.of("benchmarking_results/indexing");
+        Path sumDir  = Path.of("benchmarking_results/indexing/data");
         Files.createDirectories(outDir);
         Files.createDirectories(sumDir);
 
