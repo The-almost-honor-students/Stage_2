bc2938474734dab955d8a527042ffed71e15287a
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-10-25 23:39:36 +0100

    indexing benchmark variables

--- indexing/src/main/java/com/tahs/benchmark/IndexingBenchmarks.java
+++ indexing/src/main/java/com/tahs/benchmark/IndexingBenchmarks.java
@@ -1,7 +1,25 @@
 package com.tahs.benchmark;
 
+import com.tahs.domain.Book;
+import com.tahs.infrastructure.serialization.books.GutenbergHeaderSerializer;
+import com.tahs.infrastructure.serialization.books.TextTokenizer;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @OutputTimeUnit(TimeUnit.SECONDS)
@@ -11,6 +29,16 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Benchmark)
 
 public class IndexingBenchmarks {
+    @Param({"datalake"})
+    public String datalakeDir;
 
+    @Param({"1342"})
+    public String bookId;
+
+    private List<String> availableIds;
+    private Map<String, Set<String>> invertedIndex;
+    private Map<String, Book> metadataRepo;
+    private GutenbergHeaderSerializer headerSerializer;
+    private AtomicInteger rr;
 
 }
